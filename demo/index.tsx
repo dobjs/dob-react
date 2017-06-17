ae8091b29e3cd8ecf142ee8fbc3f771d6c48d048
ascoders <576625322@qq.com>
2017-06-17 09:25:09 +0800

    deploy

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,8 +1,42 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { inject, Container } from 'dependency-inject'
-// import { Action } from 'dynamic-object'
+import { Action, observable } from 'dynamic-object'
 import { Provider, Connect } from '../'
+
+class ArticleStore {
+    title = "我是标题党"
+}
+
+class ArticleAction {
+    @inject(ArticleStore) articleStore: ArticleStore
+
+    @Action changeTitle() {
+        this.articleStore.title = "改变了"
+    }
+}
+
+@Connect({
+    store: ArticleStore,
+    action: ArticleAction
+})
+class Article extends React.Component<any, void> {
+    componentWillMount() {
+        setTimeout(() => {
+            this.props.action.changeTitle()
+        }, 2000)
+    }
+
+    render() {
+        return (
+            <div>{this.props.store.title}</div>
+        )
+    }
+}
+
+
+
+
 
 class UserStore {
     name = 'bob'
@@ -34,18 +68,15 @@ class App extends React.Component<any, any> {
     }
 
     render() {
-        console.log('render')
+        console.log('render', this.props)
         return (
-            <div>{this.props.store.age}</div>
+            <div>
+                {this.props.store.age}
+                <Article />
+            </div>
         )
     }
 }
-
-
-
-
-
-
 
 const container = new Container()
 container.set(UserStore, new UserStore())
@@ -55,3 +86,18 @@ ReactDOM.render(
     <Provider store={container.get(UserStore)} action={container.get(UserAction)} >
         <App />
     </Provider>, document.getElementById('react-dom'))
+
+
+
+
+class Test {
+    // @Action async getUser() {
+    //     this.isLoading = true
+    //     const result = await fetch()
+    //     Action(() => {
+    //         this.isLoading = false
+    //         this.user = result
+    //     })
+    // }
+}
+
