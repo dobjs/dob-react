af78c91b06dec29d408f89fd903e3c5eedcdc4ee
ascoders <576625322@qq.com>
2017-06-17 10:45:52 +0800

    fix bug

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,103 +1,38 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { inject, Container } from 'dependency-inject'
-import { Action, observable } from 'dynamic-object'
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { Action } from 'dynamic-object'
 import { Provider, Connect } from '../'
-
-class ArticleStore {
-    title = "我是标题党"
-}
-
-class ArticleAction {
-    @inject(ArticleStore) articleStore: ArticleStore
-
-    @Action changeTitle() {
-        this.articleStore.title = "改变了"
-    }
-}
-
-@Connect({
-    store: ArticleStore,
-    action: ArticleAction
-})
-class Article extends React.Component<any, void> {
-    componentWillMount() {
-        setTimeout(() => {
-            this.props.action.changeTitle()
-        }, 2000)
-    }
-
-    render() {
-        return (
-            <div>{this.props.store.title}</div>
-        )
-    }
-}
-
-
-
-
 
 class UserStore {
     name = 'bob'
-    age = 15
 }
 
 class UserAction {
-    @inject(UserStore) store: UserStore
+    store = new UserStore()
 
-    setName(name: string) {
+    @Action setName(name: string) {
         this.store.name = name
-    }
-
-    setAge(age: number) {
-        this.store.age = age
-    }
-
-    setNickname(name: string) {
-        // this.store.nickname = name
     }
 }
 
 @Connect
 class App extends React.Component<any, any> {
-    componentWillMount() {
-        setTimeout(() => {
-            this.props.action.setAge(18)
-        }, 1000)
-    }
-
     render() {
-        console.log('render', this.props)
         return (
-            <div>
-                {this.props.store.age}
-                <Article />
-            </div>
+            <span>
+                {this.props.store.name}
+                <button onClick={() => {
+                    this.props.action.setName("jack")
+                }}>updateName</button>
+            </span>
         )
     }
 }
 
-const container = new Container()
-container.set(UserStore, new UserStore())
-container.set(UserAction, new UserAction())
+const userAction = new UserAction()
 
 ReactDOM.render(
-    <Provider store={container.get(UserStore)} action={container.get(UserAction)} >
+    <Provider action={userAction} store={userAction.store}>
         <App />
-    </Provider>, document.getElementById('react-dom'))
-
-
-
-
-class Test {
-    // @Action async getUser() {
-    //     this.isLoading = true
-    //     const result = await fetch()
-    //     Action(() => {
-    //         this.isLoading = false
-    //         this.user = result
-    //     })
-    // }
-}
-
+    </Provider>
+    , document.getElementById('react-dom'))
