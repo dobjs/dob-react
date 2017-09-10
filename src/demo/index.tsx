fe98196ffbaeccb6e4286ffd81a978e6697b9b04
ascoders <576625322@qq.com>
2017-09-10 22:33:43 +0800

    upgrade dep

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,10 +1,10 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { inject, injectFactory } from 'dependency-inject'
-import { Action, observable, useDebug } from 'dob'
+import { Action, observable, startDebug } from 'dob'
 import { Connect, Provider } from '../'
 
-useDebug()
+startDebug()
 
 function wait(time: number) {
     return new Promise(resolve => {
@@ -14,13 +14,18 @@ function wait(time: number) {
     })
 }
 
+const obj = {}
+
 const book = {
     title: 'ccc'
 }
 
 @observable
 class UserStore {
-    books = new Set([book])
+    books1 = new Set([book])
+    books2 = new Map([['book', book]])
+    books3 = new WeakSet([book])
+    books4 = new WeakMap([[obj, book]])
 }
 
 class UserAction {
@@ -28,9 +33,13 @@ class UserAction {
     userStore: UserStore
 
     @Action async setTitle() {
-        this.userStore.books.add({
+        this.userStore.books1.add({
             title: 'ddd'
         })
+        delete this.userStore.books2.get('book').title
+        this.userStore.books3.add({ a: 1 } as any)
+        this.userStore.books4.get(obj).title = '666'
+        delete this.userStore.books4.get(obj).title
     }
 }
 
@@ -40,10 +49,6 @@ class UserAction {
 }))
 class App extends React.Component<any, any> {
     render() {
-        this.props.UserStore.books.forEach((book: any) => {
-            book
-        })
-
         return (
             <div>
                 <button
